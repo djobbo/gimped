c6d56d0a7eb18c876258e5c2bda822bc1bcd209d
Djobbo <hi@djobbo.com>
2026-08-12 17:42:12 +0200

    feat(swz): add MalformedCsv/Xml/Json tagged errors

--- packages/swz/src/errors.ts
+++ packages/swz/src/errors.ts
@@ -22,3 +22,18 @@ export class IoError extends Schema.TaggedError<IoError>()("IoError", {
   path: Schema.String,
   message: Schema.String,
 }) {}
+
+export class MalformedCsv extends Schema.TaggedError<MalformedCsv>()("MalformedCsv", {
+  path: Schema.String,
+  message: Schema.String,
+}) {}
+
+export class MalformedXml extends Schema.TaggedError<MalformedXml>()("MalformedXml", {
+  path: Schema.String,
+  message: Schema.String,
+}) {}
+
+export class MalformedJson extends Schema.TaggedError<MalformedJson>()("MalformedJson", {
+  path: Schema.String,
+  message: Schema.String,
+}) {}
