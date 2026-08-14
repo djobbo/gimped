1eb814be6b6c98511cd2fa20dacb1469ebb118ab
Djobbo <hi@djobbo.com>
2026-08-14 12:42:22 +0200

    feat(patch): scaffold package with errors and registry schemas

---
+++ packages/patch/src/constants.ts
@@ -0,0 +1,7 @@
+export const STEAM_APP_ID = 291550;
+export const STEAM_DEPOT_ID = 291551;
+export const STEAM_OS = "windows";
+export const FILELIST_BODY = "regex:.*\\.swf$\nregex:.*\\.swz$\n";
+export const DEPOT_REPO = "SteamRE/DepotDownloader";
+export const JPEXS_REPO = "jindrapetrik/jpexs-decompiler";
+export const USER_AGENT = "gimped-patch";
---
+++ packages/patch/src/index.ts
@@ -0,0 +1,3 @@
+export * from "./constants.ts";
+export * from "./errors.ts";
+export * from "./schemas.ts";
